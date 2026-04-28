d65538ad4084084428bea802fcd6bf9d8056cf71
Madmax2201 <94888465+Madmax2201@users.noreply.github.com>
2026-04-28 01:20:39 +0100

    Add files via upload

---
+++ assets/LogoutPage-CG94D9O8.js
@@ -0,0 +1,1 @@
+import{r as o,Z as t}from"./index-BVG2xSIc.js";function r(){return o.useEffect(()=>{t().then(()=>{window.location.href="/login"})},[]),null}export{r as default};
---
+++ assets/mail-CehtF0Y-.js
@@ -0,0 +1,11 @@
+import{c as o}from"./createLucideIcon-CWNaxOet.js";/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],e=o("arrow-right",t);/**
+ * @license lucide-react v0.575.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7",key:"132q7q"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2",key:"izxlao"}]],i=o("mail",a);export{e as A,i as M};
